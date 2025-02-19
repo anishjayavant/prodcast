8699995cda413e61a9ee24ca87c6439667967c61
Anish Jayavant <36860630+anishjayavant@users.noreply.github.com>
2025-02-19 15:33:42 +0000

    added some traits and impls

---
+++ src/shapes/models.rs
@@ -0,0 +1,54 @@
+pub struct Circle {
+    radius: f64,
+}
+
+pub struct Rectangle {
+    width: f64,
+    height: f64,
+}
+
+#[allow(dead_code)]
+pub trait Shape {
+    fn area(&self) -> f64;
+    fn perimeter(&self) -> f64;
+}
+
+#[allow(dead_code)]
+impl Circle {
+    pub fn new(radius: f64) -> Result<Circle, String> {
+        if radius <= 0.0 {
+            return Err("Radius must be greater than 0".to_string());
+        }
+        Ok(Circle { radius })
+    }
+}
+
+#[allow(dead_code)]
+impl Rectangle {
+    pub fn new(width: f64, height: f64) -> Result<Rectangle, String> {
+        if width <= 0.0 || height <= 0.0 {
+            return Err("Width and height must be greater than 0".to_string());
+        }
+        Ok(Rectangle { width, height })
+    }
+}
+
+impl Shape for Circle {
+    fn area(&self) -> f64 {
+        std::f64::consts::PI * self.radius * self.radius
+    }
+
+    fn perimeter(&self) -> f64 {
+        2.0 * std::f64::consts::PI * self.radius
+    }
+}
+
+impl Shape for Rectangle {
+    fn area(&self) -> f64 {
+        self.width * self.height
+    }
+
+    fn perimeter(&self) -> f64 {
+        2.0 * (self.width + self.height)
+    }
+}
