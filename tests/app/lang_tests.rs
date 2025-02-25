e608bf3234cd0150e3d5ad6a9bf2d603d83c1bba
Anish Jayavant <36860630+anishjayavant@users.noreply.github.com>
2025-02-25 17:02:22 +0000

    GH-7 removed extra mod tests block

--- tests/app/lang_tests.rs
+++ tests/app/lang_tests.rs
@@ -1,22 +1,20 @@
 #[cfg(test)]
-mod tests {
-    use prodcast::app::lang::{mutate_x_ptr, mutate_x_val};
+use prodcast::app::lang::{mutate_x_ptr, mutate_x_val};
 
-    /// Tests that mutate_x_val does not change the value of x in the calling function
-    #[test]
-    fn test_mutate_x_val() {
-        let x = 10;
-        mutate_x_val(x);
-        // x should remain unchanged because mutate_x_val takes x by value
-        assert_eq!(x, 10);
-    }
+/// Tests that mutate_x_val does not change the value of x in the calling function
+#[test]
+fn test_mutate_x_val() {
+    let x = 10;
+    mutate_x_val(x);
+    // x should remain unchanged because mutate_x_val takes x by value
+    assert_eq!(x, 10);
+}
 
-    /// Tests that mutate_x_ptr changes the value of x in the calling function
-    #[test]
-    fn test_mutate_x_ptr() {
-        let mut x = 10;
-        mutate_x_ptr(&mut x);
-        // x should be incremented by 1
-        assert_eq!(x, 11);
-    }
+/// Tests that mutate_x_ptr changes the value of x in the calling function
+#[test]
+fn test_mutate_x_ptr() {
+    let mut x = 10;
+    mutate_x_ptr(&mut x);
+    // x should be incremented by 1
+    assert_eq!(x, 11);
 }
