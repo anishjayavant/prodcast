c3a2cc1ae041d284a7cbc117527ee4360aaad864
Anish Jayavant <36860630+anishjayavant@users.noreply.github.com>
2025-02-27 15:12:00 +0000

    GH-16 Switch to tracing from log in routes

--- src/routes/api.rs
+++ src/routes/api.rs
@@ -3,6 +3,7 @@ use crate::repository::newsletter::NewsletterPostGresRepository;
 use crate::service::lang::hello;
 use crate::service::newsletter::NewsletterAppService;
 use actix_web::{web, HttpRequest, HttpResponse, Responder};
+use uuid::Uuid;
 
 /// Subscribe endpoint
 /// This function will subscribe a user to the service
@@ -12,32 +13,45 @@ pub async fn subscribe(
     newsletter_app_service: web::Data<NewsletterAppService<NewsletterPostGresRepository>>,
     user: web::Form<User>,
 ) -> impl Responder {
+    // Generate a request ID
+    let request_id = Uuid::new_v4();
+    // log the request ID using tracing
+    tracing::info!(
+        "request_id: {}, adding username: {}, email: {} as a new subscriber",
+        request_id,
+        user.name(),
+        user.email()
+    );
     // unpack the form data and print
     let user_str = format!("User name: {}, email: {}", user.name(), user.email());
     let str = format!("Hello to Prodcast {}, {}", user.name(), user.email());
     // save the user to the database
-    log::info!("Saving user: {}", user_str);
+    tracing::info!("request id: {}, saving user: {}", request_id, user_str);
     newsletter_app_service
         .save_user(user.into_inner())
         .await
         // return 500 if the user could not be saved or 200 if the user was saved
         .map(|_| {
-            log::info!("User saved: {}", user_str);
+            tracing::info!("request id: {}, user saved: {}", request_id, user_str);
             HttpResponse::Ok().body(str)
         })
         .unwrap_or_else(|e| {
-            log::error!("Failed to save user: {:?}", e);
+            log::error!("request id: {}, failed to save user: {:?}", request_id, e);
             HttpResponse::InternalServerError().finish()
         })
 }
 
 /// Health check endpoint
 pub async fn healthz() -> impl Responder {
+    let request_id = Uuid::new_v4();
+    tracing::info!("request_id: {}, health check", request_id);
     HttpResponse::Ok().finish()
 }
 
 /// Greet the user
 pub async fn greet(req: HttpRequest) -> impl Responder {
+    let request_id = Uuid::new_v4();
+    tracing::info!("request_id: {}, greeting user", request_id);
     let name = req.match_info().get("name").unwrap_or("World");
     hello();
     format!("Hello {}!", &name)
